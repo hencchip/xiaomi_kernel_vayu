8be0e8b3eb3a66f8b0a6a4d59e8befa981f9dc5d
hencchip <cangaliblog@gmail.com>
2024-04-18 22:05:28 +0800

    fix: fs/aio

--- fs/aio.c
+++ fs/aio.c
@@ -1643,19 +1643,6 @@ static int io_submit_one(struct kioctx *ctx, struct iocb __user *user_iocb,
 	if (unlikely(!req))
 		return -EAGAIN;
 
-<<<<<<< HEAD
-=======
-	req->common.ki_filp = file = fget(iocb->aio_fildes);
-	if (unlikely(!req->common.ki_filp)) {
-		ret = -EBADF;
-		goto out_put_req;
-	}
-	req->common.ki_pos = iocb->aio_offset;
-	req->common.ki_complete = aio_complete;
-	req->common.ki_flags = iocb_flags(req->common.ki_filp) | IOCB_AIO_RW;
-	req->common.ki_hint = file_write_hint(file);
-
->>>>>>> c33c221f22480e9971f9416b7cedcb41ae5d393a
 	if (iocb->aio_flags & IOCB_FLAG_RESFD) {
 		/*
 		 * If the IOCB_FLAG_RESFD flag of aio_flags is set, get an
